b8f14d75ce1d9ea3f2a974fd7b66ac9afc8a76c1
Wesley Medeiros da Rosa <devwesleymedeiros@yahoo.com>
2024-11-13 18:49:51 -0300

    Aba de idiomas inserida

---
+++ assets/js/languages.js
@@ -0,0 +1,21 @@
+// const flags = [...document.querySelectorAll(".flag")];
+// flags.forEach((flag)=>{
+//     if (flag.id === "Spain") {
+        
+//     }else if(flag.id === "United-states"){
+
+//     }else if(flag.id === "Brazil"){
+
+//     }
+// })
+
+// function EnglishHtml(){
+//     document.addEventListener("DOMContentLoaded", ()=>{
+
+//     });
+// }
+// function SpanishHtml(){
+//     document.addEventListener("DOMContentLoaded", ()=>{
+        
+//     });
+// }
