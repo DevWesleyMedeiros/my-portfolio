353ec419a963d0c2fed6d7ec091d9f8e8fe6fb36
Wesley Medeiros da Rosa <devwesleymedeiros@yahoo.com>
2024-11-14 11:50:33 -0300

    Criando o layoul versão em Inglês

--- assets/js/languages.js
+++ assets/js/languages.js
@@ -1,21 +1,21 @@
-// const flags = [...document.querySelectorAll(".flag")];
-// flags.forEach((flag)=>{
-//     if (flag.id === "Spain") {
+const flags = [...document.querySelectorAll(".flag")];
+flags.forEach((flag)=>{
+    if (flag.id === "Spain") {
         
-//     }else if(flag.id === "United-states"){
+    }else if(flag.id === "United-states"){
 
-//     }else if(flag.id === "Brazil"){
+    }else if(flag.id === "Brazil"){
 
-//     }
-// })
+    }
+})
 
-// function EnglishHtml(){
-//     document.addEventListener("DOMContentLoaded", ()=>{
+function EnglishHtml(){
+    document.addEventListener("DOMContentLoaded", ()=>{
 
-//     });
-// }
-// function SpanishHtml(){
-//     document.addEventListener("DOMContentLoaded", ()=>{
+    });
+}
+function SpanishHtml(){
+    document.addEventListener("DOMContentLoaded", ()=>{
         
-//     });
-// }
+    });
+}
