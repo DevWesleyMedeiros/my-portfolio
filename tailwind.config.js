79e8f9ba65e0386e2cbc6019941472221029a44a
Wesley Medeiros da Rosa <devwesleymedeiros@yahoo.com>
2024-11-19 11:16:59 -0300

    Seção Cards criadas com sucesso

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.{html,js}", "./assets/**/*.{html,js}"], 
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
