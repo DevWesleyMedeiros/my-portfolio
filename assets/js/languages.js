1a52f9dc00ef233f5387b3990be19efa16605f0d
Wesley Medeiros da Rosa <devwesleymedeiros@yahoo.com>
2024-11-21 12:27:37 -0300

    Filtro de projeto criado

--- assets/js/languages.js
+++ assets/js/languages.js
@@ -1,9 +1,35 @@
 const flags = [...document.querySelectorAll(".country-flag > .flag")];
+const linksProjects = [...document.querySelectorAll(".portifolio > .projects-list > .filter-project > .project")];
 
 flags.forEach((flag) => {
     flag.addEventListener("click", (evt) => {
         const lang = evt.target.id;
         changeLanguage(lang);
+    });
+});
+
+linksProjects.forEach((link) => {
+    link.addEventListener("click", (evt) => {
+        const cards = [...document.querySelectorAll(".portifolio > .center > .card")];
+        let linkId = evt.target.id;
+
+        cards.forEach((card) => {
+            card.classList.remove("hidden");
+        });
+
+        if (linkId === "done") {
+            cards.forEach((card) => {
+                if (!card.classList.contains("finished")) {
+                    card.classList.add("hidden");
+                }
+            });
+        } else if (linkId === "undone") {
+            cards.forEach((card) => {
+                if (!card.classList.contains("unfinished")) {
+                    card.classList.add("hidden");
+                }
+            });
+        }
     });
 });
 
