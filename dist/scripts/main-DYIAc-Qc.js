6d7191817c433d3551bbae6a4c55485d14766835
Wesley Medeiros <devwesleymedeiros@yahoo.com>
2025-01-06 17:31:36 -0300

    Ajustado erro no caminho do  arquivo.pdf

---
+++ dist/scripts/main-DYIAc-Qc.js
@@ -0,0 +1,1 @@
+import"../chunks/submitBR-CvDJMipr.js";function t(){import.meta.url,import("_").catch(()=>1),async function*(){}().next()}export{t as __vite_legacy_guard};
---
+++ dist/scripts/main-legacy-BUCtrsGZ.js
@@ -0,0 +1,1 @@
+System.register(["../chunks/submitBR-legacy-BwPy73OD.js"],(function(e,t){"use strict";return{setters:[null],execute:function(){}}}));
