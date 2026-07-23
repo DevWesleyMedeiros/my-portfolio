ceb6c8d9805e28153c3162f7efa5800f8bc02898
Weslei Medeiros da Rosa <139824516+DevWesleyMedeiros@users.noreply.github.com>
2026-07-23 11:33:33 -0300

    chore: changes path for dir img and icons

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import react from '@vitejs/plugin-react'
-import path from 'path'
-import { fileURLToPath } from 'url'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
